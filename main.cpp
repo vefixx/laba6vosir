465297771f7f42867d7520dcf9d867f61ec3b236
vefixx <vef1xx@yandex.ru>
2025-11-20 13:39:59 +0500

    main

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "windows.h"
+
+#include "game.h"
+#include "utils.h"
+
+using namespace std;
+
+int main() {
+  SetConsoleOutputCP(CP_UTF8);
+  SetConsoleCP(CP_UTF8);
+
+  cout << "Нажмите Enter, чтобы начать игру...";
+  cin.get();
+
+  string a;
+
+  // while (true) {
+  //
+  // }
+
+  return 0;
+}
